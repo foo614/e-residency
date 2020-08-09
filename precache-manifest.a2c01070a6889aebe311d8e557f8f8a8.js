a39ad00fe0af8d5c5a979b82a946f1de0361640d
unknown <fld614@gmail.com>
2020-08-09 12:28:56 +0800

    update

---
+++ precache-manifest.a2c01070a6889aebe311d8e557f8f8a8.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a4c173fd23a34e9eb6",
+    "url": "/e-residency/css/app.c9c9056a.css"
+  },
+  {
+    "revision": "42e9d6e1fc3bab221eb6",
+    "url": "/e-residency/css/chunk-vendors.80a9e6ca.css"
+  },
+  {
+    "revision": "996d8248f580f8e26e6c45c67da9b5a6",
+    "url": "/e-residency/favicon.png"
+  },
+  {
+    "revision": "cc93a967d58329d2fd189e3476d5bb51",
+    "url": "/e-residency/img/avatar.cc93a967.jpg"
+  },
+  {
+    "revision": "2be2ff95197d8494ba877a93fc5ed995",
+    "url": "/e-residency/img/bg7.2be2ff95.jpg"
+  },
+  {
+    "revision": "8c64339c5e3e5616dd5d763626bad946",
+    "url": "/e-residency/img/christian.8c64339c.jpg"
+  },
+  {
+    "revision": "3b3fb65f693a19e0e317d13027c50b98",
+    "url": "/e-residency/img/city-profile.3b3fb65f.jpg"
+  },
+  {
+    "revision": "54f2f0923121bfbcc4523aa772cab056",
+    "url": "/e-residency/img/city.54f2f092.jpg"
+  },
+  {
+    "revision": "8f92b88bee76fc4c9b6c77c72df9894b",
+    "url": "/e-residency/img/clem-onojegaw.8f92b88b.jpg"
+  },
+  {
+    "revision": "23321ebf2db033bd157dfd5e1d16287f",
+    "url": "/e-residency/img/clem-onojeghuo.23321ebf.jpg"
+  },
+  {
+    "revision": "f7c4b2dd639f422a55ae4eb867328e82",
+    "url": "/e-residency/img/cynthia-del-rio.f7c4b2dd.jpg"
+  },
+  {
+    "revision": "5a0e395b3b76565583339619ca3110f4",
+    "url": "/e-residency/img/kendall.5a0e395b.jpg"
+  },
+  {
+    "revision": "6e91ac7f87f3ca0ef4f9161662019f12",
+    "url": "/e-residency/img/landing.6e91ac7f.jpg"
+  },
+  {
+    "revision": "2a5da8d8eef306027fa240d4e7cfc7d3",
+    "url": "/e-residency/img/leaf1.2a5da8d8.png"
+  },
+  {
+    "revision": "3b1af9dee003e5e1e2ba972d52cb3a3f",
+    "url": "/e-residency/img/leaf2.3b1af9de.png"
+  },
+  {
+    "revision": "2c91d48423cd8f957bee585266081bc7",
+    "url": "/e-residency/img/leaf3.2c91d484.png"
+  },
+  {
+    "revision": "676d933344f735d7dd6ee2ec42616454",
+    "url": "/e-residency/img/leaf4.676d9333.png"
+  },
+  {
+    "revision": "66c8b404a88a5d0bc8923314adefd2df",
+    "url": "/e-residency/img/mariya-georgieva.66c8b404.jpg"
+  },
+  {
+    "revision": "15d2ebb30a9e54dbbeae0eea100e546b",
+    "url": "/e-residency/img/olu-eletu.15d2ebb3.jpg"
+  },
+  {
+    "revision": "52e4b9baf9894423f7b11e020f04821a",
+    "url": "/e-residency/img/profile.52e4b9ba.jpg"
+  },
+  {
+    "revision": "435c148e7c651c91b5eed09eb46cb112",
+    "url": "/e-residency/img/profile_city.435c148e.jpg"
+  },
+  {
+    "revision": "bd90b5d1759ad4eef06381233ef83bcc",
+    "url": "/e-residency/img/studio-1.bd90b5d1.jpg"
+  },
+  {
+    "revision": "927b288cef97210deba1694f23f65682",
+    "url": "/e-residency/img/studio-2.927b288c.jpg"
+  },
+  {
+    "revision": "3f2427639f534e0a63eda3c50e4622da",
+    "url": "/e-residency/img/studio-3.3f242763.jpg"
+  },
+  {
+    "revision": "5bea741dbf2c3c971dedd2d836c5684d",
+    "url": "/e-residency/img/studio-4.5bea741d.jpg"
+  },
+  {
+    "revision": "5505c23bf97720d8ab82119c5b8288b7",
+    "url": "/e-residency/img/studio-5.5505c23b.jpg"
+  },
+  {
+    "revision": "e481c0e38f6759647f05bac43f957cd3",
+    "url": "/e-residency/img/vue-mk-header.e481c0e3.jpg"
+  },
+  {
+    "revision": "4b87f32711df0c40e59b590a16333a27",
+    "url": "/e-residency/index.html"
+  },
+  {
+    "revision": "a4a4c173fd23a34e9eb6",
+    "url": "/e-residency/js/app.cb911928.js"
+  },
+  {
+    "revision": "42e9d6e1fc3bab221eb6",
+    "url": "/e-residency/js/chunk-vendors.3daaf17b.js"
+  },
+  {
+    "revision": "1936999255286bf0aec26354c3fd9746",
+    "url": "/e-residency/manifest.json"
+  }
+]);
